39778c0d7a8f5e4aba711b314ac416c7b2e91d68
Willson Emmett <royalguy1031@gmail.com>
2023-02-23 10:52:54 +0900

    ...

--- src/env.js
+++ src/env.js
@@ -5,7 +5,6 @@ export const OBJECT_TAB = "object";
 
 export const TREASURY_ID_RAFFLE = "000";
 export const TREASURY_ID_SWAP = "000";
-export const TREASURY_ID = "0.0.1833905";
 
 export const NETWORK_TYPE = "mainnet";
 export const MIRROR_NET_URL = "https://mainnet-public.mirrornode.hedera.com";
@@ -13,12 +12,6 @@ export const GET_ACCOUNT_PREFIX = "/api/v1/accounts/";
 export const IPFS_URL = "https://hashpack.b-cdn.net/ipfs/";
 
 export const COINGECKO_URL = "https://api.coingecko.com";
-
-export const SERVER_URL = "http://95.217.63.153:3306";
-export const IMG_URL = "http://95.217.63.153:3306/images";
-
-// export const SERVER_URL = "https://degenland.tech:3306";
-// export const IMG_URL = "https://degenland.tech:3306/images";
 
 export const DEGENLAND_NFT_ID = "0.0.1783975";
 export const TYCOON_NFT_ID = "0.0.1467455";
