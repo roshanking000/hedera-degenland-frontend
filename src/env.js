acc1036558c1b626be34ed251fca304835f63597
Willson Emmett <royalguy1031@gmail.com>
2023-03-16 11:20:24 +0900

    modify env.js

--- src/env.js
+++ src/env.js
@@ -14,11 +14,11 @@ export const IPFS_URL = "https://hashpack.b-cdn.net/ipfs/";
 
 export const COINGECKO_URL = "https://api.coingecko.com";
 
-export const SERVER_URL = "http://localhost:5000";
-export const IMG_URL = "http://localhost:5000/images";
+// export const SERVER_URL = "http://localhost:5000";
+// export const IMG_URL = "http://localhost:5000/images";
 
-// export const SERVER_URL = "https://degenland.tech:3306";
-// export const IMG_URL = "https://degenland.tech:3306/images";
+export const SERVER_URL = "https://degenland.tech:3306";
+export const IMG_URL = "https://degenland.tech:3306/images";
 
 export const DEGENLAND_NFT_ID = "0.0.1783975";
 export const TYCOON_NFT_ID = "0.0.1467455";
